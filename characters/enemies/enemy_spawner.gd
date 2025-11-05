0555e16fb9e55698d796ad1f0e32358a1874a46e
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2025-11-05 22:32:04 +0100

    Added randomness variables to the enemy spawner

--- characters/enemies/enemy_spawner.gd
+++ characters/enemies/enemy_spawner.gd
@@ -6,6 +6,8 @@ extends Node3D
 #
 class_name EnemySpawner
 
+var rng := RandomNumberGenerator.new()
+
 @export_category("Spawn Properties")
 @export var enemy_scene: PackedScene
 @export var spawn_delay := 3.0
@@ -14,8 +16,24 @@ class_name EnemySpawner
 # The maximum time the spawner can be active
 @export var max_spawn_time := 20.0
 
+@export_category("Spawn Randomness")
+@export var position_randomness := Vector3.ZERO
+@export var max_enemies_randomness := 0 :
+	set(value):
+		assert(value>=0)
+		max_enemies_randomness = value
+		max_enemies_spawned += rng.randi_range(0, value)
+@export var spawn_delay_randomness := 0.0 :
+	set(value):
+		assert(value>=0)
+		if(spawn_timer):
+			spawn_timer.wait_time = spawn_delay + rng.randf_range(-value, value)
+		spawn_delay_randomness = value
+
 @onready var auto_remove_timer: Timer = $AutoRemoveTimer
 @onready var spawn_timer: Timer = $SpawnTimer
+
+
 
 var enemies_spawned := 0:
 	set(value):
@@ -27,9 +45,23 @@ var enemies_spawned := 0:
 func _ready() -> void:
 	auto_remove_timer.wait_time = max_spawn_time
 	spawn_timer.wait_time = spawn_delay
+	spawn_timer.timeout.connect(
+		func():
+			spawn_timer.wait_time = (
+				spawn_delay + 
+				rng.randf_range(-spawn_delay_randomness, spawn_delay_randomness)
+				)
+	)
 	
 func spawn_enemy():
 	assert(enemy_scene)
-	var instance := enemy_scene.instantiate()
+	var instance:Node3D = enemy_scene.instantiate()
+	var pos := Vector3(
+		rng.randf_range(-1, 1),
+		rng.randf_range(-1, 1), 
+		rng.randf_range(-1, 1),
+		)
+	pos *= 2.0 * position_randomness
+	instance.position = pos
 	get_parent().add_child(instance)
-	enemies_spawned+=1
+	enemies_spawned += 1
--- utils/enemy_waves/spawn_spawner_event.gd
+++ utils/enemy_waves/spawn_spawner_event.gd
@@ -11,6 +11,9 @@ class_name SpawnSpawnerEvent
 @export var spawn_delay := 3.0
 @export var max_enemies_spawned := 10
 @export var max_spawn_time := 20.0
+@export var position_randomness := Vector3.ZERO
+@export var max_enemies_randomness := 0
+@export var spawn_delay_randomness := 0.0
 
 func execute(_wave_handler: EnemyWaveHandler):
 	assert(spawner_scene)
@@ -22,5 +25,8 @@ func execute(_wave_handler: EnemyWaveHandler):
 	spawner_instance.spawn_delay = spawn_delay
 	spawner_instance.max_enemies_spawned = max_enemies_spawned
 	spawner_instance.max_spawn_time = max_spawn_time
+	spawner_instance.position_randomness = position_randomness
+	spawner_instance.max_enemies_randomness = max_enemies_randomness
+	spawner_instance.spawn_delay_randomness = spawn_delay_randomness
 	
 	_wave_handler.add_child(spawner_instance)
