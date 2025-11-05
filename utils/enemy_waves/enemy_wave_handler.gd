95192e2e576cdeebbfbb02eda95be51ec154594c
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2025-11-05 19:27:49 +0100

    Added signal for when EnemyWave is finnished (no events left)

--- utils/enemy_waves/enemy_wave_handler.gd
+++ utils/enemy_waves/enemy_wave_handler.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 class_name EnemyWaveHandler
 
+signal last_event_executed
+
 @export var events: Array[EnemyWaveEvent]
 
 var time := 0.0
@@ -15,12 +17,12 @@ func _ready() -> void:
 
 # Returns true if first event was executed and removed
 func try_execute_first_event() -> bool:
-	if(events && !events.is_empty()):
-		var curr := events[0]
-		if(curr.event_time <= time):
-			curr.execute(self)
-			events.remove_at(0)
-			return true
+	if(events && !events.is_empty() && events.front().event_time <= time):
+		events.front().execute(self)
+		events.remove_at(0)
+		if(events.is_empty()):
+			last_event_executed.emit()
+		return true
 	return false
 		
 
